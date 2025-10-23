f2ed8dea7096d8a92539543e067a1de7bfe57e0d
Kashif <casprian@gmail.com>
2025-10-23 10:01:46 +0530

    configuring test environment and test cases

--- src/components/ProjectCard/ProjectCard.test.jsx
+++ src/components/ProjectCard/ProjectCard.test.jsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ProjectCard from './ProjectCard';
 
+
 const mockProject = {
   show: true,
   title: 'Test Project',
